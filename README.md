9bc19d6529982324d5f922438f9e2cf88b7fa07e
Zoki Spasov <zoki@adventgroup.net>
2022-01-26 17:30:58 +0200

    Changed the module name

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## Colored Messages
 
-colored-messages is a minimalist python library which gives you the possibility to print in color in the CLI.
+delicate is a minimalist python library which gives you the possibility to print in color in the CLI.
 
 ## Install
 
-`pip install colored-messages`
+`pip3 install delicate`
 
 ## Usage
 
 ```python
-import colored-messages as messages
+import delicate as messages
 
 messages.log('This is my log message', 'RED')
--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="colored-messages",
+    name="delicate",
     version="1.0.0",
     author="Advent Group",
     author_email="development@adventgroup.net",
     description="Printing colored messages to the CLI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/adventgroup/colored-messages",
+    url="https://github.com/adventgroup/delicate",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
